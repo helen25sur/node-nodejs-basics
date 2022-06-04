8748cbb7ff9fdd7c1344783464d4a1f1eb03f2bc
Olena Surilova <elen21surilova@gmail.com>
2022-06-05 00:11:54 +0300

    feat: add code for the function hash/calcHash.js

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,3 +1,21 @@
+import { createReadStream } from 'fs';
+const { createHash } = await import('crypto');
+import { stdout } from 'process';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const pathToFile = path.join(__dirname, 'files', 'fileToCalculateHashFor.txt');
+
 export const calculateHash = async () => {
-    // Write your code here 
+  try {
+    const hash = await createHash('SHA256');
+    const input = createReadStream(pathToFile);
+    input.pipe(hash).setEncoding('hex').pipe(stdout);
+  } catch (error) {
+    console.log(error.message);
+  }
 };
+
+calculateHash();
