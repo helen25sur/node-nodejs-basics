b5aa3c596ff4b862c0220930892b1b3ccf43b1d6
Olena Surilova <elen21surilova@gmail.com>
2022-06-04 17:21:49 +0300

    feat: add code for the function fs/read.js

--- src/fs/read.js
+++ src/fs/read.js
@@ -1,3 +1,24 @@
+import {
+  readFile
+} from 'fs/promises';
+import path from 'path';
+import {
+  fileURLToPath
+} from 'url';
+
+const __filename = fileURLToPath(
+  import.meta.url);
+const __dirname = path.dirname(__filename);
+const pathToFile = path.join(__dirname, 'files', 'fileToRead.txt');
+
 export const read = async () => {
-    // Write your code here 
+
+  try {
+    const readed = await readFile(pathToFile);
+    await console.log(readed.toString());
+  } catch {
+    throw new Error('FS operation failed');
+  }
 };
+
+read();
