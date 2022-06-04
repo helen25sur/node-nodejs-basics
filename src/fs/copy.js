0c0946cec768698585d33d0a7b12682145ff648a
Olena Surilova <elen21surilova@gmail.com>
2022-06-04 15:50:02 +0300

    fix: change code for the function fs/copy.js

--- src/fs/copy.js
+++ src/fs/copy.js
@@ -1,11 +1,7 @@
 import {
-  constants
-} from 'fs';
-import {
   mkdir,
   copyFile,
-  readdir,
-  access
+  readdir
 } from 'fs/promises';
 import path from 'path';
 import {
@@ -19,29 +15,21 @@ const pathToDefaultFolder = path.join(__dirname, 'files');
 const pathToNewFolder = path.join(__dirname, 'files_copy');
 
 export const copy = async () => {
+
   try {
-    await access(pathToNewFolder);
-    throw new Array('FS operation failed');
-  } catch (err) {
-    if (err['0'] !== undefined) {
-      console.error(err['0']);
-      process.exit();
+    await mkdir(pathToNewFolder);
+    const files = await readdir(pathToDefaultFolder, {
+      withFileTypes: true
+    });
+    for (const file of files) {
+      const pathToDefaultFile = path.join(pathToDefaultFolder, file.name);
+      const pathToNewFile = path.join(pathToNewFolder, file.name);
+      await copyFile(pathToDefaultFile, pathToNewFile);
     }
+    console.log('All files copied!');
+  } catch {
+    throw new Error('FS operation failed');
   }
-
-  await mkdir(pathToNewFolder, {
-    recursive: true
-  });
-  const files = await readdir(pathToDefaultFolder, {
-    withFileTypes: true
-  });
-  for (const file of files) {
-    const pathToDefaultFile = path.join(pathToDefaultFolder, file.name);
-    const pathToNewFile = path.join(pathToNewFolder, file.name);
-
-    await copyFile(pathToDefaultFile, pathToNewFile, constants.COPYFILE_FICLONE);
-  }
-  console.log('All files copied!');
 };
 
 copy();
