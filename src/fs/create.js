e3b35752571146060e7e6f37afd4b9a3cffdbf58
Olena Surilova <elen21surilova@gmail.com>
2022-06-04 13:12:38 +0300

    feat: add code for the function fs/create.js

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,3 +1,30 @@
+import {
+  writeFile,
+  access
+} from "fs/promises";
+import path from 'path';
+import {
+  fileURLToPath
+} from 'url';
+
+const __filename = fileURLToPath(
+  import.meta.url);
+const __dirname = path.dirname(__filename);
+const pathToFile = path.join(__dirname, 'files', 'fresh.txt');
+
 export const create = async () => {
-    // Write your code here 
+  try {
+    await access(pathToFile);
+    throw new Array('FS operation failed');
+  } catch (err) {
+    if (err['0'] !== undefined) {
+      console.error(err['0']);
+      process.exit();
+    }
+  }
+  await writeFile(pathToFile, 'I am fresh and young',
+    console.log('File created')
+  );
 };
+
+create();
