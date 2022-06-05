dca1210aa90543b60050a27fee3400b280390c36
Olena Surilova <elen21surilova@gmail.com>
2022-06-05 14:13:13 +0300

    feat: add code for the function streams/transform.js

--- src/streams/transform.js
+++ src/streams/transform.js
@@ -1,3 +1,13 @@
+import { Transform } from 'stream';
+
 export const transform = async () => {
-    // Write your code here 
+  const reverseString = new Transform({
+    transform(chunk, encoding, cb) {
+      this.push(chunk.toString().split('').reverse().join(''));
+      cb();
+    },
+  });
+
+  process.stdin.pipe(reverseString).pipe(process.stdout);
 };
+transform();
